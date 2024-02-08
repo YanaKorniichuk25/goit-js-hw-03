2d39893016de0efa9199eaed9956c1c7ae978075
YanaKorniichuk25 <149394343+YanaKorniichuk25@users.noreply.github.com>
2024-02-08 14:00:32 +0100

    hw js 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,14 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding);
-  const borderWidth = parseFloat(border);
-  return contentWidth + 2 * paddingWidth + 2 * borderWidth;
+function filterArray(numbers, value) {
+  const array = [];
+  for (const element of numbers) {
+    if (element > value) {
+      array.push(element);
+    }
+  }
+  return array;
 }
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
